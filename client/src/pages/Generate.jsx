f835bee821f75020ee4664d793078ab72e64c114
Slowikxx <aniakslowik@student.agh.edu.pl>
2023-06-20 03:41:58 +0200

    Image generating

--- client/src/pages/Generate.jsx
+++ client/src/pages/Generate.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { getRandomPrompt } from '../utils';
 import { Loading } from '../components';
+
 const Generate = () => {
 	const [generating, setGenerating] = useState(false);
 	const [loading, setLoading] = useState(false);
@@ -18,20 +19,80 @@ const Generate = () => {
 		setForm({ ...form, prompt: rndPrompt });
 	};
 
+	const generateImg = async () => {
+		if (form.prompt) {
+			try {
+				setGenerating(true);
+				const response = await fetch('http://localhost:8000/api/v1/ai', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						prompt: form.prompt,
+					}),
+				});
+
+				const data = await response.json();
+				setForm({ ...form, image: `data:image/jpeg;base64,${data.image}` });
+			} catch (err) {
+				alert(err);
+			} finally {
+				setGenerating(false);
+			}
+		} else {
+			alert('Please provide a proper prompt');
+		}
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (form.prompt && form.image) {
+			setLoading(true);
+			try {
+				const response = await fetch(`http://localhost:8000/api/v1/post`, {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({ ...form }),
+				});
+
+				await response.json();
+				alert('Success');
+			} catch (err) {
+				alert(err);
+			} finally {
+				setLoading(false);
+			}
+		} else {
+			alert('Please provide a prompt to generate');
+		}
+	};
+
 	return (
-		<div className="home-gradient md:h-[77.5vh] h-[80.1vh] flex items-center z-0 flex-col">
+		<form
+			onSubmit={handleSubmit}
+			className="home-gradient md:h-[77.5vh] h-[80.1vh] flex items-center z-0 flex-col"
+		>
 			<div className="flex flex-col w-full px-5">
-				<label className="text-[30px] text-[#855E5E] md:self-auto self-center">
+				<label
+					htmlFor="prompt"
+					className="text-[30px] text-[#855E5E] md:self-auto self-center"
+				>
 					Enter a detailed prompt
 				</label>
 				<input
 					type="text"
+					name="prompt"
+					id="prompt"
 					value={form.prompt}
+					onChange={handleChange}
 					className="md:w-[1500px] md:h-[60px] w-[450px] bg-[#ECE0E0] dark:bg-[#313338] rounded-[15px] outline-none border-none text-[15px] font-['Arial'] p-2 self-center"
 				/>
 			</div>
 			<div className="md:flex hidden flex-row justify-between w-full overflow-hidden h-full px-40 py-5">
-				<div className="w-[800px] h-[400px] bg-[#ECE0E0] dark:bg-[#313338] self-end rounded-[10px] flex justify-center items-center">
+				<div className="w-[384px] h-[384px] bg-[#ECE0E0] dark:bg-[#313338] self-end rounded-[10px] flex justify-center items-center">
 					{form.image ? (
 						<img
 							src={form.image}
@@ -48,12 +109,17 @@ const Generate = () => {
 						You don’t have any idea? Try this:
 					</p>
 					<button
+						type="button"
 						onClick={handleRandom}
 						className="bg-[#7B2789] text-[#ECE0E0] dark:text-[#313338] w-[220px] h-[60px] rounded-[10px]"
 					>
 						CLICK ON ME!
 					</button>
-					<button className="self-end mt-[230px] w-[150px] h-[60px] text-[#ECE0E0] dark:text-[#313338] bg-[#AD2121] rounded-[10px]">
+					<button
+						type="submit"
+						onClick={generateImg}
+						className="self-end mt-[230px] w-[150px] h-[60px] text-[#ECE0E0] dark:text-[#313338] bg-[#AD2121] rounded-[10px]"
+					>
 						{generating ? 'GENERATING...' : 'GENERATE'}
 					</button>
 				</div>
@@ -64,17 +130,33 @@ const Generate = () => {
 					You don’t have any idea? Try this:
 				</p>
 				<button
+					type="button"
 					onClick={handleRandom}
 					className="w-[400px] h-[40px] text-[20px] text-[#ECE0E0] dark:text-[#313338] bg-[#7B2789] rounded-[10px]"
 				>
 					CLICK ON ME!
 				</button>
-				<div className="w-[400px] h-[250px] bg-[#ECE0E0] dark:bg-[#313338] rounded-[10px]" />
-				<button className="w-[400px] h-[40px] text-[20px] text-[#ECE0E0] dark:text-[#313338] bg-[#AD2121] rounded-[10px]">
+				<div className="w-[256px] h-[256px] bg-[#ECE0E0] dark:bg-[#313338] rounded-[10px]">
+					{form.image ? (
+						<img
+							src={form.image}
+							alt={form.prompt}
+							className="w-full h-full object-contain rounded-[10px]"
+						/>
+					) : (
+						''
+					)}
+					{generating && <Loading />}
+				</div>
+				<button
+					type="submit"
+					onClick={generateImg}
+					className="w-[400px] h-[40px] text-[20px] text-[#ECE0E0] dark:text-[#313338] bg-[#AD2121] rounded-[10px]"
+				>
 					{generating ? 'GENERATING...' : 'GENERATE'}
 				</button>
 			</div>
-		</div>
+		</form>
 	);
 };
 
